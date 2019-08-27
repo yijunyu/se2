080fd4b088662985aa015e2d0ed208c311d4e796
Yijun Yu <y.yu@open.ac.uk>
2019-08-27 20:34:22 +0800

    adding the visualization example

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,5 @@ RUN wget https://github.com/bazelbuild/bazel/releases/download/0.28.1/bazel-0.28
 RUN chmod +x bazel-0.28.1-installer-linux-x86_64.sh \
  && ./bazel-0.28.1-installer-linux-x86_64.sh --user
 RUN apt-get install -y clang
+RUN apt-get install -y graphviz
 USER gitpod
--- ex-bazel/run.sh
+++ ex-bazel/run.sh
@@ -3,3 +3,15 @@ if [ ! -d bazel ]; then
 fi
 cd bazel/examples/cpp
 bazel build
+cd -
+###
+# for visualization, see https://blog.bazel.build/2015/06/17/visualize-your-build.html
+if [ ! -d tiny-workspace ]; then
+	git clone https://github.com/kchodorow/tiny-workspace
+fi
+cd tiny-workspace
+echo query --package_path $(pwd):$HOME/bin/base_workspace > ~/.bazelrc
+bazel query 'deps(//:main)' --output graph > graph.in
+dot -Tpng < graph.in > graph.png
+gp open graph.png
+cd -
