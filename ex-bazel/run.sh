0e5de1464ee8fef58c11656962c2456db5ffafc0
Yijun Yu <y.yu@open.ac.uk>
2019-08-28 02:27:10 +0000

    remove visualization

--- ex-bazel/run.sh
+++ ex-bazel/run.sh
@@ -4,15 +4,3 @@ fi
 cd bazel/examples/cpp
 bazel build
 cd -
-###
-# for visualization, see https://blog.bazel.build/2015/06/17/visualize-your-build.html
-if [ ! -d tiny-workspace ]; then
-	git clone https://github.com/kchodorow/tiny-workspace
-fi
-cd tiny-workspace
-echo query --package_path $(pwd):$HOME/bin/base_workspace > ~/.bazelrc
-#bazel query 'deps(//:main)' --output graph > graph.in
-bazel query --noimplicit_deps 'deps(//:main)' --output graph > graph.in
-dot -Tpng < graph.in > graph.png
-gp open graph.png
-cd -
